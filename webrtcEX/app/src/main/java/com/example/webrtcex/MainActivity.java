0bebe64ceec0e8659985794c444c95633f7db5a5
shawn <echun1234@gmail.com>
2020-05-12 21:34:59 +0900

    권한 추가 및 메소드 분리

--- webrtcEX/app/src/main/java/com/example/webrtcex/MainActivity.java
+++ webrtcEX/app/src/main/java/com/example/webrtcex/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.webrtcex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
+import org.json.JSONObject;
 import org.webrtc.PeerConnection;
 import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SurfaceTextureHelper;
 import org.webrtc.VideoCapturer;
 import org.webrtc.AudioSource;
 import org.webrtc.AudioTrack;
@@ -22,70 +33,148 @@ import org.webrtc.VideoCapturer;
 import org.webrtc.VideoRenderer;
 import org.webrtc.VideoSource;
 import org.webrtc.VideoTrack;
+import org.webrtc.PeerConnectionFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SignallingClient.SignalingInterface {
+
+
+    PeerConnectionFactory peerConnectionFactory;
+    MediaConstraints audioConstraints;
+    MediaConstraints videoConstraints;
+    MediaConstraints sdpConstraints;
+    VideoSource videoSource;
+    VideoTrack localVideoTrack;
+    AudioSource audioSource;
+    AudioTrack localAudioTrack;
+    SurfaceTextureHelper surfaceTextureHelper;
+
+    SurfaceViewRenderer localVideoView;
+    SurfaceViewRenderer remoteVideoView;
+
+    Button hangup;
+    PeerConnection localPeer;
+    //List<IceServer> iceServers;
+    EglBase rootEglBase;
+
+    boolean gotUserMedia;
+    List<PeerConnection.IceServer> peerIceServers = new ArrayList<>();
+
+    final int ALL_PERMISSIONS_CODE = 1;
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PeerConnectionFactory.initializeAndroidGlobals(this,true,true,true);
+        //권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, ALL_PERMISSIONS_CODE);
+        } else {
+            start();
+        }
 
-        PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
-        PeerConnectionFactory peerConnectionFactory = new PeerConnectionFactory(options);
+    }
 
-        VideoCapturer videoCapturerAndroid = createVideoCapturer();
+    private void initViews() {
+        hangup = findViewById(R.id.end_call);
+        localVideoView = findViewById(R.id.local_gl_surface_view);
+        remoteVideoView = findViewById(R.id.remote_gl_surface_view);
+        hangup.setOnClickListener(this);
+    }
 
-        MediaConstraints constraints = new MediaConstraints();
+    private void initVideos() {
+        rootEglBase = EglBase.create();
+        localVideoView.init(rootEglBase.getEglBaseContext(), null);
+        remoteVideoView.init(rootEglBase.getEglBaseContext(), null);
+        localVideoView.setZOrderMediaOverlay(true);
+        remoteVideoView.setZOrderMediaOverlay(true);
+    }
 
-        VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturerAndroid);
-        VideoTrack localVideoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
+    public void start(){
+        //화면이 계속 켜져있게 함
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        AudioSource audioSource = peerConnectionFactory.createAudioSource(constraints);
-        AudioTrack localAudioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
+        initViews();    //뷰의 요소들 초기화
+        initVideos();   //비디오 요소들 초기화, webrtc관련 요소
 
-        videoCapturerAndroid.startCapture(1000, 1000, 30);
+        //socket관련, emit해주고 이런거 ㅇㅇ
+        SignallingClient.getInstance().init(this);
 
-        SurfaceViewRenderer videoView = (SurfaceViewRenderer) findViewById(R.id.surface_rendeer);
-        videoView.setMirror(true);
+        //peerconectionfactory 초기화
+        PeerConnectionFactory.InitializationOptions initializationOptions =
+                PeerConnectionFactory.InitializationOptions.builder(this)
+                        .createInitializationOptions();
 
-        EglBase rootEglBase = EglBase.create();
-        videoView.init(rootEglBase.getEglBaseContext(), null);
+        PeerConnectionFactory.initialize(initializationOptions);
 
-        localVideoTrack.addRenderer(new VideoRenderer(videoView));
+
     }
 
 
-    private VideoCapturer createVideoCapturer() {
-        VideoCapturer videoCapturer;
-        videoCapturer = createCameraCapturer(new Camera1Enumerator(false));
-        return videoCapturer;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == ALL_PERMISSIONS_CODE
+                && grantResults.length == 2
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            // all permissions granted
+            start();
+        } else {
+            finish();
+        }
     }
 
-    private VideoCapturer createCameraCapturer(CameraEnumerator enumerator) {
-        final String[] deviceNames = enumerator.getDeviceNames();
+    @Override
+    public void onClick(View v) {
 
-        for (String deviceName : deviceNames) {
-            if (enumerator.isFrontFacing(deviceName)) {
-                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+    }
 
-                if (videoCapturer != null) {
-                    return videoCapturer;
-                }
-            }
-        }
+    @Override
+    public void onRemoteHangUp(String msg) {
 
-        for (String deviceName : deviceNames) {
-            if (!enumerator.isFrontFacing(deviceName)) {
-                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
-                if (videoCapturer != null) {
-                    return videoCapturer;
-                }
-            }
-        }
+    }
 
-        return null;
+    @Override
+    public void onOfferReceived(JSONObject data) {
+
+    }
+
+    @Override
+    public void onAnswerReceived(JSONObject data) {
+
+    }
+
+    @Override
+    public void onIceCandidateReceived(JSONObject data) {
+
+    }
+
+    @Override
+    public void onTryToStart() {
+
+    }
+
+    @Override
+    public void onCreatedRoom() {
+
+    }
+
+    @Override
+    public void onJoinedRoom() {
+
+    }
+
+    @Override
+    public void onNewPeerJoined() {
+
     }
 }
