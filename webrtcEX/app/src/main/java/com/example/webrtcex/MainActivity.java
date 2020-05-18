2d107de6a1a302e7bf1112c9a89afd57f9a5427d
shawn <echun1234@gmail.com>
2020-05-18 20:21:26 +0900

    연결을 위한 일련의 과정 공부

--- webrtcEX/app/src/main/java/com/example/webrtcex/MainActivity.java
+++ webrtcEX/app/src/main/java/com/example/webrtcex/MainActivity.java
@@ -9,13 +9,24 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.webrtc.DefaultVideoDecoderFactory;
+import org.webrtc.DefaultVideoEncoderFactory;
+import org.webrtc.IceCandidate;
+import org.webrtc.MediaStream;
 import org.webrtc.PeerConnection;
 import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SessionDescription;
 import org.webrtc.SurfaceTextureHelper;
 import org.webrtc.VideoCapturer;
 import org.webrtc.AudioSource;
@@ -115,10 +126,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         PeerConnectionFactory.initialize(initializationOptions);
 
 
+        //인코더 디코더 이용하는 peerConnectionFactory 인스턴스 생성
+        PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
+
+        DefaultVideoEncoderFactory defaultVideoEncoderFactory = new DefaultVideoEncoderFactory(
+                rootEglBase.getEglBaseContext(),  /* enableIntelVp8Encoder */true,  /* enableH264HighProfile */true);
+        DefaultVideoDecoderFactory defaultVideoDecoderFactory = new DefaultVideoDecoderFactory(rootEglBase.getEglBaseContext());
+        peerConnectionFactory = PeerConnectionFactory.builder()
+                .setOptions(options)
+                .setVideoEncoderFactory(defaultVideoEncoderFactory)
+                .setVideoDecoderFactory(defaultVideoDecoderFactory)
+                .createPeerConnectionFactory();
+
+        //비디오 캡처 인스턴스
+        VideoCapturer videoCapturerAndroid;
+        videoCapturerAndroid = createCameraCapturer(new Camera1Enumerator(false));
+
+        //mediaConstraints생성
+        audioConstraints = new MediaConstraints();
+        videoConstraints = new MediaConstraints();
+
+        //비디오 소스 인스턴스 생성
+        if (videoCapturerAndroid != null) {
+            surfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", rootEglBase.getEglBaseContext());
+            videoSource = peerConnectionFactory.createVideoSource(videoCapturerAndroid.isScreencast());
+            videoCapturerAndroid.initialize(surfaceTextureHelper, this, videoSource.getCapturerObserver());
+        }
+        localVideoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
+
+
+        //오디오
+        audioSource = peerConnectionFactory.createAudioSource(audioConstraints);
+        localAudioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
+
+        //비디오캡처 시작
+        if (videoCapturerAndroid != null) {
+            videoCapturerAndroid.startCapture(1024, 720, 30);
+        }
+
+        localVideoView.setVisibility(View.VISIBLE);
+
+        //비디오 랜더준비 비디오트렉에 랜더 추가
+        localVideoTrack.addSink(localVideoView);
+
+        localVideoView.setMirror(true);
+        remoteVideoView.setMirror(true);
+
+        gotUserMedia = true;
+        if (SignallingClient.getInstance().isInitiator) {
+            onTryToStart();
+        }
     }
 
-
-    @Override
+    //권한 검사
+   @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
@@ -126,55 +187,279 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 && grantResults.length == 2
                 && grantResults[0] == PackageManager.PERMISSION_GRANTED
                 && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-            // all permissions granted
+            // 모든 권한이 허가됐을 시
             start();
         } else {
             finish();
         }
     }
 
-    @Override
-    public void onClick(View v) {
-
-    }
-
-    @Override
-    public void onRemoteHangUp(String msg) {
-
-    }
-
-    @Override
-    public void onOfferReceived(JSONObject data) {
-
-    }
-
-    @Override
-    public void onAnswerReceived(JSONObject data) {
-
-    }
-
-    @Override
-    public void onIceCandidateReceived(JSONObject data) {
-
-    }
-
+    //이 앱이 initiator이거나 로컬미디아를 가지고 있을때 호출되거나  원격 피어가 동영상을 보낼준비가 됐다는걸 소켓을 통해 보낼 때
     @Override
     public void onTryToStart() {
-
+        runOnUiThread(() -> {
+            if (!SignallingClient.getInstance().isStarted && localVideoTrack != null && SignallingClient.getInstance().isChannelReady) {
+                createPeerConnection();
+                SignallingClient.getInstance().isStarted = true;
+                if (SignallingClient.getInstance().isInitiator) {
+                    doCall();
+                }
+            }
+        });
     }
 
+    // local peerconnection 인스턴스를 만듦
+    private void createPeerConnection() {
+        PeerConnection.RTCConfiguration rtcConfig =
+                new PeerConnection.RTCConfiguration(peerIceServers);
+        // TCP candidates are only useful when connecting to a server that supports
+        // ICE-TCP.
+        rtcConfig.tcpCandidatePolicy = PeerConnection.TcpCandidatePolicy.DISABLED;
+        rtcConfig.bundlePolicy = PeerConnection.BundlePolicy.MAXBUNDLE;
+        rtcConfig.rtcpMuxPolicy = PeerConnection.RtcpMuxPolicy.REQUIRE;
+        rtcConfig.continualGatheringPolicy = PeerConnection.ContinualGatheringPolicy.GATHER_CONTINUALLY;
+        // Use ECDSA encryption.
+        rtcConfig.keyType = PeerConnection.KeyType.ECDSA;
+        localPeer = peerConnectionFactory.createPeerConnection(rtcConfig, new CustomPeerConnectionObserver("localPeerCreation") {
+            @Override
+            public void onIceCandidate(IceCandidate iceCandidate) {
+                super.onIceCandidate(iceCandidate);
+                onIceCandidateReceived(iceCandidate);
+            }
+
+            @Override
+            public void onAddStream(MediaStream mediaStream) {
+                showToast("Received Remote stream");
+                super.onAddStream(mediaStream);
+                gotRemoteStream(mediaStream);
+            }
+        });
+
+        addStreamToLocalPeer();
+    }
+
+
+    //localpeer에 스트림 추가
+    private void addStreamToLocalPeer() {
+        //local mediastream 생성
+        MediaStream stream = peerConnectionFactory.createLocalMediaStream("102");
+        stream.addTrack(localAudioTrack);
+        stream.addTrack(localVideoTrack);
+        localPeer.addStream(stream);
+    }
+
+
+    //앱이 initiator일때 호출됨, 오퍼를 생성하고 소켓을 통해 전송할꺼
+    private void doCall() {
+        sdpConstraints = new MediaConstraints();
+        sdpConstraints.mandatory.add(
+                new MediaConstraints.KeyValuePair("OfferToReceiveAudio", "true"));
+        sdpConstraints.mandatory.add(
+                new MediaConstraints.KeyValuePair("OfferToReceiveVideo", "true"));
+        localPeer.createOffer(new CustomSdpObserver("localCreateOffer") {
+            @Override
+            public void onCreateSuccess(SessionDescription sessionDescription) {
+                super.onCreateSuccess(sessionDescription);
+                localPeer.setLocalDescription(new CustomSdpObserver("localSetLocalDesc"), sessionDescription);
+                Log.d("onCreateSuccess", "SignallingClient emit ");
+                SignallingClient.getInstance().emitMessage(sessionDescription);
+            }
+        }, sdpConstraints);
+    }
+
+
+    //원격피어의 미디어스트림을 받고 첫번째 비디오트랙을 레더링할거임
+    private void gotRemoteStream(MediaStream stream) {
+        //we have remote video stream. add to the renderer.
+        final VideoTrack videoTrack = stream.videoTracks.get(0);
+        runOnUiThread(() -> {
+            try {
+                remoteVideoView.setVisibility(View.VISIBLE);
+                videoTrack.addSink(remoteVideoView);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    //local ice candidate를 받음, negotiation을 위한 신호로 원격피어에 전달함
+    public void onIceCandidateReceived(IceCandidate iceCandidate) {
+        //we have received ice candidate. We can set it to the other peer.
+        SignallingClient.getInstance().emitIceCandidate(iceCandidate);
+    }
+
+    //SignallingCallback 룸이 처음만들어질때 호출됨, 첨만들어지는건 내가 initiator라는 뜻
     @Override
     public void onCreatedRoom() {
-
+        showToast("You created the room " + gotUserMedia);
+        if (gotUserMedia) {
+            SignallingClient.getInstance().emitMessage("got user media");
+        }
     }
 
+    //SignallingCallback 룸에 들어갈때 호출됨, 그말은 내가 participant라는 뜻
     @Override
     public void onJoinedRoom() {
-
+        showToast("You joined the room " + gotUserMedia);
+        if (gotUserMedia) {
+            SignallingClient.getInstance().emitMessage("got user media");
+        }
     }
 
     @Override
     public void onNewPeerJoined() {
+        showToast("Remote Peer Joined");
+    }
 
+    @Override
+    public void onRemoteHangUp(String msg) {
+        showToast("Remote Peer hungup");
+        runOnUiThread(this::hangup);
+    }
+
+
+    //SignallingCallback 원격피어가 오퍼를 받을때 호출됨
+    @Override
+    public void onOfferReceived(final JSONObject data) {
+        showToast("Received Offer");
+        runOnUiThread(() -> {
+            if (!SignallingClient.getInstance().isInitiator && !SignallingClient.getInstance().isStarted) {
+                onTryToStart();
+            }
+
+            try {
+                localPeer.setRemoteDescription(new CustomSdpObserver("localSetRemote"), new SessionDescription(SessionDescription.Type.OFFER, data.getString("sdp")));
+                doAnswer();
+                updateVideoViews(true);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void doAnswer() {
+        localPeer.createAnswer(new CustomSdpObserver("localCreateAns") {
+            @Override
+            public void onCreateSuccess(SessionDescription sessionDescription) {
+                super.onCreateSuccess(sessionDescription);
+                localPeer.setLocalDescription(new CustomSdpObserver("localSetLocal"), sessionDescription);
+                SignallingClient.getInstance().emitMessage(sessionDescription);
+            }
+        }, new MediaConstraints());
+    }
+
+    //SignallingCallback 원격피어가 내 오퍼에 답을 보낼때 호출 됨
+    @Override
+    public void onAnswerReceived(JSONObject data) {
+        showToast("Received Answer");
+        try {
+            localPeer.setRemoteDescription(new CustomSdpObserver("localSetRemote"), new SessionDescription(SessionDescription.Type.fromCanonicalForm(data.getString("type").toLowerCase()), data.getString("sdp")));
+            updateVideoViews(true);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //원격 icecandidate를 받음
+    @Override
+    public void onIceCandidateReceived(JSONObject data) {
+        try {
+            localPeer.addIceCandidate(new IceCandidate(data.getString("id"), data.getInt("label"), data.getString("candidate")));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateVideoViews(final boolean remoteVisible) {
+        runOnUiThread(() -> {
+            ViewGroup.LayoutParams params = localVideoView.getLayoutParams();
+            if (remoteVisible) {
+                params.height = dpToPx(100);
+                params.width = dpToPx(100);
+            } else {
+                params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            }
+            localVideoView.setLayoutParams(params);
+        });
+    }
+
+
+    //정상적 종료나 앱을 지울 때
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.end_call: {
+                hangup();
+                break;
+            }
+        }
+    }
+
+    private void hangup() {
+        try {
+            if (localPeer != null) {
+                localPeer.close();
+            }
+            localPeer = null;
+            SignallingClient.getInstance().close();
+            updateVideoViews(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        SignallingClient.getInstance().close();
+        super.onDestroy();
+
+        if (surfaceTextureHelper != null) {
+            surfaceTextureHelper.dispose();
+            surfaceTextureHelper = null;
+        }
+    }
+
+
+
+    // 유틸 메소드들
+    public int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    public void showToast(final String msg) {
+        runOnUiThread(() -> Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show());
+    }
+
+    private VideoCapturer createCameraCapturer(CameraEnumerator enumerator) {
+        final String[] deviceNames = enumerator.getDeviceNames();
+
+        //전면카메라부터 찾아봄
+        Logging.d(TAG, "Looking for front facing cameras.");
+        for (String deviceName : deviceNames) {
+            if (enumerator.isFrontFacing(deviceName)) {
+                Logging.d(TAG, "Creating front facing camera capturer.");
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        //없으면 다른 카메라를 찾아야지
+        Logging.d(TAG, "Looking for other cameras.");
+        for (String deviceName : deviceNames) {
+            if (!enumerator.isFrontFacing(deviceName)) {
+                Logging.d(TAG, "Creating other camera capturer.");
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        return null;
     }
 }
